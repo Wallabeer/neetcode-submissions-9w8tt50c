53b784c67eda35d5c3732ad4d2e55c81fbcd2b57
Wallabeer <19523863+Wallabeer@users.noreply.github.com>
2026-04-23 22:51:29 +0800

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(word)
+        
+        rows = len(board)
+        cols = len(board[0])
+        
+        def helper(row, col, i):
+            if i == n:
+                return True
+
+            if row >= rows or row < 0:
+                return
+            if col >= cols or col < 0:
+                return
+            
+            if (row, col) in path:
+                return
+
+            if board[row][col] != word[i]:
+                return
+
+            path.add((row, col))
+            
+            result = (
+                        helper(row+1, col, i+1)
+                        or helper(row-1, col, i+1)
+                        or helper(row, col+1, i+1)
+                        or helper(row, col-1, i+1)
+                    )
+            if result:
+                return True
+            else:
+                path.remove((row, col))
+                return result
+            
+        start = word[0]
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] != start:
+                    continue
+                    
+                path = set()
+                if helper(row,col,0):
+                    return True
+        return False
+            
